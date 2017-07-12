6e1dd309a2303ce201f8712c5832be92e6eecbf2
Dustin <dyelland@gmail.com>
2017-07-12 11:42:07 -0500

    fixed the non working project rendering

--- scripts/model.js
+++ scripts/model.js
@@ -1,25 +1,26 @@
 'use strict';
 
-function Project(name, img, url, repoUrl, contributors) {
-  this.name = projectsData.name;
-  this.img = projectsData.img;
-  this.url = projectsData.url;
-  this.repoUrl = projectsData.repoUrl;
-  this.contributors = projectsData.contributors;
+function Project(object) {
+  this.name = object.name;
+  this.img = object.img;
+  this.repoUrl = object.repoUrl;
+  this.url = object.url;
+  this.contributors = object.contributors;
 }
 
 var projects = [];
 
-projectsData.forEach(function(projectsData){
-  console.log(projectsData);
-  projects.push(new Project(projectsData));
+projectsData.forEach(function(projectObject){
+  console.log(projectObject);
+  projects.push(new Project(projectObject));
   console.log(projects);
 });
 
 var $projects = $('.projectscontainer');
-projects.forEach(function(){
-  $projects.append('<h2>' + projects.name + '</h2>',
-  '<a href="' + projects.url + '">See Project Here</a>',
-  '<a href="' + projects.repoUrl + '">See Repository Here</a>'
+projects.forEach(function(object){
+  $projects.append('<h2>' + object.name + '</h2>',
+  '<img src="' + object.img + '"/>',
+  '<a href="' + object.url + '">See Project Here</a>',
+  '<a href="' + object.repoUrl + '">See Repository Here</a>'
   );
 });
--- scripts/rawData.js
+++ scripts/rawData.js
@@ -1,16 +1,23 @@
 var projectsData = [{
   name: 'Check In',
-  img: 'tbd',
+  img: 'img/CheckIn.png',
   url: 'https://dustinyschild.github.io/hotel-final-project/',
   repoUrl: 'https://github.com/dustinyschild/hotel-final-project',
-  contributers: 'Dustin, Benjamin, Chuck'
+  contributors: 'Dustin(me), Benjamin, Chuck'
 },
 {
   name: 'Cookie Stand',
-  img: 'tbd',
+  img: 'img/SalmonCookies.png',
   url: 'https://dustinyschild.github.io/hotel-final-project/',
   repoUrl: 'https://github.com/dustinyschild/hotel-final-project',
-  contributers: 'Dustin'
+  contributors: 'Dustin(me)'
+},
+{
+  name: 'About Me',
+  img: 'img/AboutMe.png',
+  url: 'https://dustinyschild.github.io/about_me/',
+  repoUrl: 'https://github.com/dustinyschild/about_me',
+  contributors: 'Dustin(me)'
 }];
 console.log(projectsData);
 console.log('rawData rendered');
