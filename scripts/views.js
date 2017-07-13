32c46fe2ee2170bd3f86734081892785f03879dd
Dustin <dyelland@gmail.com>
2017-07-12 22:37:21 -0500

    page now shows one tab at a time

--- scripts/views.js
+++ scripts/views.js
@@ -9,5 +9,12 @@ $(document).ready(function(){
 viewObject.navHandler = function(){
   $('.hamburger-nav').on('click', 'li.tab', function(item){
     console.log(this);
+    $('section').hide();
+    console.log($('section'));
+    var activeTab = $(this).data('content');
+    var sectionId = '#' + activeTab;
+    $(sectionId).fadeIn();
+
   });
+  $('.hamburger-nav .tab:first').click();
 };
