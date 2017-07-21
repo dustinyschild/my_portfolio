abf6f209200ed2964eae21df9c3cc66a4d71e59d
Meepedy <david@psyon.org>
2017-07-21 09:00:36 -0500

    added comments

--- scripts/model.js
+++ scripts/model.js
@@ -16,6 +16,7 @@ Project.prototype.toHtml = function() {
   return template(this);
 };
 
+//loops through rawData pulled from the JSON and created a new Project for each item.
 Project.loadAll = function(rawData) {
   rawData.forEach(function(ele) {
     projects.push(new Project(ele));
@@ -26,18 +27,19 @@ var rawData;
 Project.fetchAll = function() {
 
   if (localStorage.rawData) {
-
+    //if the localStorage was found it loads it in.
     console.log('localStorage data found');
     rawData = JSON.parse(localStorage.getItem('rawData'));
     Project.loadAll(rawData);
     // You need to do Something.initIndexPage();
 
   } else {
-
+    //if localStorage wasnt found it submits an ajax request for the json file
     console.log("Local storage data not found");
     $.getJSON('data/rawData.json').done(function(data){
       rawData = data;
       console.log(rawData);
+      //stores the json to localStorage for reuse
       localStorage.setItem('rawData', JSON.stringify(rawData));
       Proejct.loadAll(rawData);
       // You need to do Something.initIndexPage();
