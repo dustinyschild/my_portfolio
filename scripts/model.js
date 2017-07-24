297ba76fe4c694483e34c4dd4618d8008d541267
Dustin <dyelland@gmail.com>
2017-07-24 16:03:04 -0500

    saving current changes

--- scripts/model.js
+++ scripts/model.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var projects = [];
+Project.all = [];
 
 function Project(object) {
   this.name = object.name;
@@ -13,36 +13,30 @@ function Project(object) {
 Project.prototype.toHtml = function() {
   var source = $('#template-project').text();
   var template = Handlebars.compile(source);
-  console.log('handlebars template: ',this);
   return template(this);
 };
 
-rawData.forEach(function(projectObject){
-  projects.push(new Project(projectObject));
-});
 
 projects.forEach(function(project){
   $('#projects').append(project.toHtml());
 });
 
 Project.loadAll = function(rawData) {
-  rawData.sort(function(a,b) {
-    return (new Date(b.publishedOn)) - (new Date(a.publishedOn));
-  });
 
   rawData.forEach(function(ele) {
     Project.all.push(new Project(ele));
+    console.log(Project.all);
   });
 };
 
 function fetchAll(){
   if (localStorage.data){
     Project.loadAll(JSON.parse(localStorage.data));
+    console.log('localStorage exists.');
   } else {
-    var rawData = $.getJSON('data/hackerIpsum.json', function(data){
-      localStorage.data('data', JSON.stringify(data));
-      Project.loadAll(data);
-    });
+  $.getJSON('data/hackerIpsum.json', function(data){
+    localStorage.data('data', JSON.stringify(data));
+    Project.loadAll(localStorage.data);
+  });
   }
-  return rawData;
 }
